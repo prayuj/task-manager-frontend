30f0d8bc173b1f20f6fdbe16f53dd993923e3d4a
AzureAD\PrayujPillai <prayuj.pillai@pens.com>
2021-04-11 22:35:14 +0530

    LETS START MAKING THIS FRONTEND!

---
+++ src/components/privateRoute.js
@@ -0,0 +1,24 @@
+import {
+    Route,
+    Redirect
+} from "react-router-dom";
+const PrivateRoute = ({ children, ...rest }) => {
+    const auth = {}
+    return (<Route
+        {...rest}
+        render={({ location }) =>
+            auth.user ? (
+                children
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: "/login",
+                        state: { from: location }
+                    }}
+                />
+            )
+        }
+    />);
+}
+
+export default PrivateRoute;
